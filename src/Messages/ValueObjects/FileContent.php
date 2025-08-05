c41ecca9b9b3d361b3bd6e4bd967da16816f1872
Joseph G <gabito.joseph@gmail.com>
2025-08-05 14:32:41 +0800

    Use MessagePart::KEY_FILE in FileContent toArray
    Replaces the use of getMessagePartType()->value with the constant MessagePart::KEY_FILE in the FileContent::toArray() method for improved consistency and clarity.

--- src/Messages/ValueObjects/FileContent.php
+++ src/Messages/ValueObjects/FileContent.php
@@ -6,6 +6,7 @@ namespace WordPress\AiClient\Messages\ValueObjects;
 
 use WordPress\AiClient\Files\DTO\File;
 use WordPress\AiClient\Messages\Contracts\MessageContentInterface;
+use WordPress\AiClient\Messages\DTO\MessagePart;
 use WordPress\AiClient\Messages\Enums\MessagePartTypeEnum;
 use WordPress\AiClient\Messages\ValueObjects\ContentGettersTrait;
 
@@ -76,6 +77,6 @@ final class FileContent implements MessageContentInterface
      */
     public function toArray(): array
     {
-        return [ $this->getMessagePartType()->value => $this->file->toArray() ];
+        return [MessagePart::KEY_FILE => $this->file->toArray()];
     }
 }
