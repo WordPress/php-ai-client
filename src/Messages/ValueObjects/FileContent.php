9cb2c8270b8b1a3e1915b1f05e7adc07cf97bcd3
Joseph G <gabito.joseph@gmail.com>
2025-08-05 12:42:22 +0800

    Add FileContent value object for message files
    Introduces the FileContent class implementing MessageContentInterface to encapsulate file content as a value object. Provides methods for retrieving the file, its type, and converting the content to an array.

---
+++ src/Messages/ValueObjects/FileContent.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WordPress\AiClient\Messages\ValueObjects;
+
+use WordPress\AiClient\Files\DTO\File;
+use WordPress\AiClient\Messages\Contracts\MessageContentInterface;
+use WordPress\AiClient\Messages\Enums\MessagePartTypeEnum;
+
+/**
+ * Value object representing file content.
+ *
+ * This immutable value object encapsulates file content and provides
+ * convenient methods for accessing and manipulating it.
+ *
+ * @since n.e.x.t
+ */
+final class FileContent implements MessageContentInterface
+{
+    /**
+     * The file content.
+     *
+     * @since n.e.x.t
+     */
+    private File $file;
+
+    /**
+     * Constructor.
+     *
+     * @since n.e.x.t
+     *
+     * @param File $file The file content.
+     */
+    public function __construct(File $file)
+    {
+        $this->file = $file;
+    }
+
+    /**
+     * Gets the type of the file content.
+     *
+     * @since n.e.x.t
+     *
+     * @return MessagePartTypeEnum Instance of the 'FILE' type.
+     */
+    public function getMessagePartType(): MessagePartTypeEnum
+    {
+        return MessagePartTypeEnum::file();
+    }
+
+    /**
+     * Gets the file content.
+     *
+     * @since n.e.x.t
+     *
+     * @return File The file content.
+     */
+    public function getFile(): File
+    {
+        return $this->file;
+    }
+
+    /**
+     * Converts the file content to an array.
+     *
+     * @since n.e.x.t
+     *
+     * @return array The file content as an array.
+     */
+    public function toArray(): array
+    {
+        return [ $this->getMessagePartType()->value => $this->file->toArray() ];
+    }
+}
