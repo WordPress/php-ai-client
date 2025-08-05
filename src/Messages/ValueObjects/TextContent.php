3165307ad90573aec3dd7a1b29f773e655956b15
Joseph G <gabito.joseph@gmail.com>
2025-08-05 12:42:39 +0800

    Add TextContent value object for message text
    Introduces the TextContent class implementing MessageContentInterface, encapsulating immutable text content with methods for type retrieval and array conversion. This supports structured handling of message text parts.

---
+++ src/Messages/ValueObjects/TextContent.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WordPress\AiClient\Messages\ValueObjects;
+
+use WordPress\AiClient\Messages\Contracts\MessageContentInterface;
+use WordPress\AiClient\Messages\Enums\MessagePartTypeEnum;
+
+/**
+ * Value object representing text content.
+ *
+ * This immutable value object encapsulates text content and provides
+ * convenient methods for accessing and manipulating it.
+ *
+ * @since n.e.x.t
+ */
+final class TextContent implements MessageContentInterface
+{
+    /**
+     * The text content.
+     *
+     * @since n.e.x.t
+     */
+    private string $text;
+
+    /**
+     * Constructor.
+     *
+     * @since n.e.x.t
+     *
+     * @param string $text The text content.
+     */
+    public function __construct(string $text)
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * Gets the type of the text content.
+     *
+     * @since n.e.x.t
+     *
+     * @return MessagePartTypeEnum Instance of the 'TEXT' type.
+     */
+    public function getMessagePartType(): MessagePartTypeEnum
+    {
+        return MessagePartTypeEnum::text();
+    }
+
+    /**
+     * Gets the text content.
+     *
+     * @since n.e.x.t
+     *
+     * @return string The text content.
+     */
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    /**
+     * Converts the text content to an array.
+     *
+     * @since n.e.x.t
+     *
+     * @return array The text content as an array.
+     */
+    public function toArray(): array
+    {
+        return [$this->getMessagePartType()->value => $this->text];
+    }
+}
