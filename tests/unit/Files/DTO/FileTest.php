4a606f052432f6b56773956bf2e11ca67214c967
Joseph G <gabito.joseph@gmail.com>
2025-08-05 10:58:23 +0800

    Add isType assertions to FileTest for video files
    Extended the FileTest unit test to include assertions for the isType method, specifically verifying correct type detection for 'video', 'image', 'audio', and 'text'.

--- tests/unit/Files/DTO/FileTest.php
+++ tests/unit/Files/DTO/FileTest.php
@@ -207,6 +207,10 @@ class FileTest extends TestCase
         $this->assertFalse($file->isImage());
         $this->assertFalse($file->isAudio());
         $this->assertFalse($file->isText());
+        $this->assertTrue($file->isType('video'));
+        $this->assertFalse($file->isType('image'));
+        $this->assertFalse($file->isType('audio'));
+        $this->assertFalse($file->isType('text'));
     }
 
     /**
