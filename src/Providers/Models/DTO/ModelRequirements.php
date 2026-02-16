865c02470536b794b26480ca162a474aa316686c
Jason Adams <jason.adams@automattic.com>
2026-02-16 14:06:34 -0700

    fix: uses client exception instead of generic

--- src/Providers/Models/DTO/ModelRequirements.php
+++ src/Providers/Models/DTO/ModelRequirements.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace WordPress\AiClient\Providers\Models\DTO;
 
-use InvalidArgumentException;
+use WordPress\AiClient\Common\Exception\InvalidArgumentException;
 use WordPress\AiClient\Common\AbstractDataTransferObject;
 use WordPress\AiClient\Messages\DTO\Message;
 use WordPress\AiClient\Messages\Enums\ModalityEnum;
