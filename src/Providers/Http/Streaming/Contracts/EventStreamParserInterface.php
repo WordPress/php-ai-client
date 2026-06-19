998d6958fb027f2b0414f158ec25174ca7ff3e5e
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-06-19 13:21:17 +0530

    Add interface for sse parser

---
+++ src/Providers/Http/Streaming/Contracts/EventStreamParserInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WordPress\AiClient\Providers\Http\Streaming\Contracts;
+
+use Psr\Http\Message\StreamInterface;
+use WordPress\AiClient\Providers\Http\Streaming\ServerSentEvent;
+
+/**
+ * Decodes a response body stream into discrete events.
+ *
+ * @since n.e.x.t
+ */
+interface EventStreamParserInterface
+{
+    /**
+     * Lazily decodes the given stream into events as they arrive.
+     *
+     * @since n.e.x.t
+     *
+     * @param StreamInterface $stream The response body stream.
+     * @return iterable<ServerSentEvent> The decoded events.
+     */
+    public function parse(StreamInterface $stream): iterable;
+}
